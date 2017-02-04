8d2100fce98d1b03af99b59699cd4ce1ba9d5ddf
Aleks Rebkavets <tarpach@yandex.ru>
2017-02-05 01:06:19 +0300

    Guessing game

---
+++ src/guessing-game.js
@@ -0,0 +1,43 @@
+class GuessingGame {
+    constructor(min, max, mid, number, guess) {
+    	this.min = min;
+    	this.max = max;
+    	this.mid = mid;
+    	this.number = number;
+    	this.guess = guess;
+    }
+
+    setRange(min, max) {
+    	min = 0;
+    	max = 3445;
+
+    }
+
+    guess() {
+    	
+    	var guess = prompt("Guess a number between 1 and 3445!");
+    	if (guess < number) {
+    		console.log("Too small!");
+    	};
+    	if (guess > number) {
+    		console.log("Too big!");
+    	};
+      if (guess === number) { 
+      	console.log("The number is found. Congratulations!");
+      };
+    }
+
+    lower() {
+    	if (guess < number) {
+           return mid + 1;
+    	}
+   }
+    greater() {
+    	if (guess > number) {
+    		return mid -1;
+    	}
+
+    }
+}
+
+module.exports = GuessingGame;
